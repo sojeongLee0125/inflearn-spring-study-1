727494c3202aa9d27201564036e5d4e28dd27fde
sojeonglee <sojeonglee0125@gmail.com>
2022-10-02 16:50:58 +0900

    [update] 도메인 객체 생성 및 Repository 인터페이스/구현체 생성

---
+++ src/test/java/spring/springstudy/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,24 @@
+package spring.springstudy.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
