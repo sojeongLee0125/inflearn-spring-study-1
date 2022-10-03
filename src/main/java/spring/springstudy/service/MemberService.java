0e96bda252677872ee3ba04c8d32ee4e780ccf92
sojeonglee <sojeonglee0125@gmail.com>
2022-10-03 14:29:53 +0900

    [update] service 클래스 작성 완료

---
+++ src/main/java/spring/springstudy/service/MemberService.java
@@ -0,0 +1,36 @@
+package spring.springstudy.service;
+
+import spring.springstudy.domain.Member;
+import spring.springstudy.repository.MemberRepository;
+import spring.springstudy.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 검사
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
