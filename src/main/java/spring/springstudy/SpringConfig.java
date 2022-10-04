9ab4c7783102b1b0cdbfa70d869f755d393da916
sojeonglee <sojeonglee0125@gmail.com>
2022-10-04 20:45:34 +0900

    [update] jdbcRepository 변경 주석 추가

--- src/main/java/spring/springstudy/SpringConfig.java
+++ src/main/java/spring/springstudy/SpringConfig.java
@@ -25,6 +25,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
+        // 스프링의 DI (Dependencies Injection)을 사용하면 기존 코드를 전혀 손대지 않고,
+        // 설정만으로 구현 클래스를 변경할 수 있다.
+        // return new MemoryMemberRepository();
         return new JdbcMemberRepository(dataSource);
     }
 }
