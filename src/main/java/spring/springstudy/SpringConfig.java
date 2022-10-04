2b196bf515003d2d3739861844bc1da21d02b2e4
sojeonglee <sojeonglee0125@gmail.com>
2022-10-04 21:06:48 +0900

    [update] jdbcTemplateMemberRepository 작성 및 설정 코드 변경

--- src/main/java/spring/springstudy/SpringConfig.java
+++ src/main/java/spring/springstudy/SpringConfig.java
@@ -3,6 +3,7 @@ package spring.springstudy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.springstudy.repository.JdbcMemberRepository;
+import spring.springstudy.repository.JdbcTemplateMemberRepository;
 import spring.springstudy.repository.MemberRepository;
 import spring.springstudy.repository.MemoryMemberRepository;
 import spring.springstudy.service.MemberService;
@@ -28,6 +29,7 @@ public class SpringConfig {
         // 스프링의 DI (Dependencies Injection)을 사용하면 기존 코드를 전혀 손대지 않고,
         // 설정만으로 구현 클래스를 변경할 수 있다.
         // return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
