032448055b0797d8aca78925cb46f485dc1dae0f
sojeonglee <sojeonglee0125@gmail.com>
2022-10-04 15:25:53 +0900

    [update] 회원 가입, 회원 목록 페이지 구현

--- src/main/java/spring/springstudy/controller/MemberController.java
+++ src/main/java/spring/springstudy/controller/MemberController.java
@@ -2,7 +2,15 @@ package spring.springstudy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring.springstudy.domain.Member;
+import spring.springstudy.domain.MemberForm;
 import spring.springstudy.service.MemberService;
+
+import java.awt.event.MouseEvent;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +20,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
